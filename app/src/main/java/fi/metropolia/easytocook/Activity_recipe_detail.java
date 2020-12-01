966a8d7149cdfe606c4d3db8144f19bb2576b680
Dongbin Yang <dongbin.yang@metropolia.fi>
2020-12-01 18:23:32 +0200

    Recipe detail UI page completed

---
+++ app/src/main/java/fi/metropolia/easytocook/Activity_recipe_detail.java
@@ -0,0 +1,14 @@
+package fi.metropolia.easytocook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_recipe_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
