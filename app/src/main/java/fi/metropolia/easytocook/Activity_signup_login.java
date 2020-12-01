31410ac9bd4d392b6d06d60e38b2d4194e30c047
Ekaterina Volkova <ekaterina.volkova@metropolia.fi>
2020-12-01 11:48:29 +0300

    from scratcj signup

--- app/src/main/java/fi/metropolia/easytocook/Activity_signup_login.java
+++ app/src/main/java/fi/metropolia/easytocook/Activity_signup_login.java
@@ -5,24 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_signup_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup_login);
 
-
-        //add intent to direct to signup/Login page
-        findViewById(R.id.btnWelcome).setOnClickListener(new View.OnClickListener() {
+        //add intent to direct to Login page
+        findViewById(R.id.btnLog_In).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Activity_signup_login.class);
+                Intent intent = new Intent(getApplicationContext(), Activity_logIn.class);
                 startActivity(intent);
             }
         });
-
     }
 }
---
+++ app/src/main/java/fi/metropolia/easytocook/Activity_welcome.java
@@ -0,0 +1,18 @@
+package fi.metropolia.easytocook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity_welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
